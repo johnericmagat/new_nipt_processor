87dd02e0bfe22f0a58854d3bcaa1f1934c981c0d
John Eric Magat <johnericmagat@gmail.com>
2022-09-15 15:50:13 +0800

    Import excel implementation

--- new_nipt_processor/MainWindow.xaml.cs
+++ new_nipt_processor/MainWindow.xaml.cs
@@ -1,15 +1,17 @@
-﻿//using new_nipt_processor.BAL;
+﻿using Microsoft.Office.Interop.Excel;
+using Microsoft.Win32;
+using new_nipt_processor.BAL;
 using new_nipt_processor.Helper;
-//using new_nipt_processor.Model;
 using Squirrel;
 using System;
 using System.Configuration;
 using System.Data;
-using System.Globalization;
-using System.Linq;
+using System.Data.OleDb;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Threading;
+using Application = Microsoft.Office.Interop.Excel.Application;
+using DataTable = System.Data.DataTable;
+using Window = System.Windows.Window;
 
 namespace new_nipt_processor
 {
@@ -18,6 +20,12 @@ namespace new_nipt_processor
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private string excelFileLocation;
+		private string excelFilename;
+
+		DataTable excelReservations;
+		DataTable tableReservations;
+
 		public MainWindow()
 		{
 			InitializeComponent();
@@ -55,7 +63,34 @@ namespace new_nipt_processor
 		{
 			try
 			{
-				
+				GetReservationsExcel();
+				GetReservationsTable();
+
+				foreach (DataRow row in excelReservations.Select())
+				{
+					foreach (DataRow row2 in tableReservations.Select())
+					{
+						try
+						{
+							if (row["illumina_report_id"].ToString().Equals(row2["illumina_report_id"].ToString()))
+							{
+								excelReservations.Rows.Remove(row);
+								excelReservations.AcceptChanges();
+								break;
+							}
+						}
+						catch
+						{
+						}
+					}
+				}
+
+
+
+
+
+
+
 			}
 			catch (Exception ex)
 			{
@@ -63,9 +98,94 @@ namespace new_nipt_processor
 			}
 		}
 
+		private	void GetReservationsExcel()
+		{
+			excelReservations = new DataTable();
+
+			string excelConnection = @"Provider=Microsoft.ACE.OLEDB.12.0; Data Source={0}; Extended Properties=Excel 12.0;";
+			excelConnection = String.Format(excelConnection, excelFileLocation);
+
+			using (OleDbConnection connection = new OleDbConnection(excelConnection))
+			{
+				connection.Open();
+
+				OleDbCommand command = new OleDbCommand("SELECT * From [Sheet1$]", connection);
+				OleDbDataAdapter adapter = new OleDbDataAdapter(command);
+				adapter.Fill(excelReservations);
+			}
+		}
+
+		private void GetReservationsTable()
+		{
+			DateTime ds = Convert.ToDateTime(DtpDateStart.Text.ToString());
+			DateTime de = Convert.ToDateTime(DtpDateEnd.Text.ToString());
+
+			tableReservations = ReservationsBAL.FilterUsers(ds.ToString("yyyy-MM-dd"), de.ToString("yyyy-MM-dd"));
+		}
+
+		private void CreateExcelFile(DataTable content)
+		{
+			SaveFileDialog saveFileDialog = new SaveFileDialog();
+			saveFileDialog.FileName = "NotExist";
+			saveFileDialog.DefaultExt = ".xlsx";
+			saveFileDialog.Filter = "Excel Files (*.xlsx)|*.xlsx";
+
+			Nullable<bool> result = saveFileDialog.ShowDialog();
+			if (result == true)
+			{
+				Application app = new Application();
+				Workbook wb = app.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
+				Worksheet ws = wb.Worksheets[1];
+				ws.Range["A1"].Value = "EmployeeId";
+				ws.Range["B1"].Value = "Fullname";
+				ws.Range["C1"].Value = "Address";
+				ws.Range["D1"].Value = "ContactNo";
+				ws.Range["E1"].Value = "Email";
+				ws.Range["F1"].Value = "DateOfBirth";
+				ws.Range["G1"].Value = "Height";
+				ws.Range["H1"].Value = "Weight";
+				ws.Range["I1"].Value = "Company";
+
+				if (content.Rows.Count > 0)
+				{
+					int cnt = 1;
+					foreach (DataRow employee in content.Rows)
+					{
+						ws.Range["A" + (cnt + 1).ToString()].Value = employee[0].ToString();
+						ws.Range["B" + (cnt + 1).ToString()].Value = employee[1].ToString();
+						ws.Range["C" + (cnt + 1).ToString()].Value = employee[2].ToString();
+						ws.Range["D" + (cnt + 1).ToString()].Value = employee[3].ToString();
+						ws.Range["E" + (cnt + 1).ToString()].Value = employee[4].ToString();
+						ws.Range["F" + (cnt + 1).ToString()].Value = employee[5].ToString();
+						ws.Range["G" + (cnt + 1).ToString()].Value = employee[6].ToString();
+						ws.Range["H" + (cnt + 1).ToString()].Value = employee[7].ToString();
+						ws.Range["I" + (cnt + 1).ToString()].Value = employee[8].ToString();
+						cnt++;
+					}
+				}
+				wb.SaveAs(saveFileDialog.FileName, XlFileFormat.xlWorkbookDefault, Type.Missing,
+					Type.Missing, false, false, XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+				wb.Close();
+
+				MessageBox.Show("Successfully created on file path " + saveFileDialog.FileName, "CREATE FILE", MessageBoxButton.OK, MessageBoxImage.Information);
+			}
+		}
+
 		private void BtnOpen_Click(object sender, RoutedEventArgs e)
 		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
 
+			openFileDialog.DefaultExt = ".xlsx";
+			openFileDialog.Filter = "Excel Files (*.xlsx)|*.xlsx";
+
+			Nullable<bool> result = openFileDialog.ShowDialog();
+			if (result == true)
+			{
+				string filename = openFileDialog.FileName;
+				TxtFilePath.Text = filename;
+				excelFileLocation = filename;
+				excelFilename = filename.Split('\\')[filename.Split('\\').Length - 1];
+			}
 		}
 
 		private void BtnProcess_Click(object sender, RoutedEventArgs e)
@@ -84,7 +204,7 @@ namespace new_nipt_processor
 				MessageBoxButton.YesNo, MessageBoxImage.Question);
 			if (result == MessageBoxResult.Yes)
 			{
-				Application.Current.Shutdown();
+				System.Windows.Application.Current.Shutdown();
 			}
 		}
 	}
